c20f39abf64c3f414b49c791e1af0ef5cddf56a1
Brandon Mork <bpmork@ncsu.edu>
2016-02-08 10:54:03 -0500

    Added abc import and ABCMeta to visualizer

--- drgraph/visualizer.py
+++ drgraph/visualizer.py
@@ -1,4 +1,7 @@
+from abc import ABCMeta, abstractmethod
+
 class StageVisualizer(object):
     """
     Abstract class that handles visualization for a particular step.
     """
+    __metaclass__ = ABCMeta
