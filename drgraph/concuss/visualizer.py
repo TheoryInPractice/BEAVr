5aaab20036b0b9b4ab845bc490d4a1f32514edda
yho <yho@ncsu.edu>
2016-02-08 10:37:04 -0500

    Added empty classes to drgraph.concuss.visualizer

---
+++ drgraph/concuss/visualizer.py
@@ -0,0 +1,14 @@
+from drgraph.visualizer import StageVisualizer
+
+class ColorVisualizer(StageVisualizer):
+    """Visualization for CONCUSS coloring stage"""
+
+class DecomposeVisualizer(StageVisualizer):
+    """Visualization for CONCUSS decomposition stage"""
+
+class CountVisualizer(StageVisualizer):
+    """Visualization for CONCUSS counting stage"""
+
+class CombineVisualizer(StageVisualizer):
+    """Visualization for CONCUSS combination stage"""
+
---
+++ drgraph/visualizer.py
@@ -0,0 +1,4 @@
+class StageVisualizer(object):
+    """
+    Abstract class that handles visualization for a particular step.
+    """
