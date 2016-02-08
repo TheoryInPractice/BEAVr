92c6b853c17abb4e2b6b53347645c30ab202d6ee
yho <yho@ncsu.edu>
2016-02-08 10:54:40 -0500

    Added initializer to visualizer base class

--- drgraph/visualizer.py
+++ drgraph/visualizer.py
@@ -1,4 +1,10 @@
-class StageVisualizer(object):
+import wx
+
+class StageVisualizer(wx.Panel):
     """
     Abstract class that handles visualization for a particular step.
     """
+
+    def __init__(self, parent):
+        """Creates a window to display the visuals in"""
+        super(StageVisualizer, self).__init__(parent) 
