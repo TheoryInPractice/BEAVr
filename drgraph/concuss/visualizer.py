e6495f099039fa308a1e73371f3edeb253467170
yho <yho@ncsu.edu>
2016-02-08 10:48:28 -0500

    Added initializers for concuss visualizers.

--- drgraph/concuss/visualizer.py
+++ drgraph/concuss/visualizer.py
@@ -3,12 +3,24 @@ from drgraph.visualizer import StageVisualizer
 class ColorVisualizer(StageVisualizer):
     """Visualization for CONCUSS coloring stage"""
 
+    def __init__(self):
+        super(ColorVisualizer, self).__init__()
+
 class DecomposeVisualizer(StageVisualizer):
     """Visualization for CONCUSS decomposition stage"""
+
+    def __init__(self):
+        super(DecomposeVisualizer, self).__init__()
 
 class CountVisualizer(StageVisualizer):
     """Visualization for CONCUSS counting stage"""
 
+    def __init__(self):
+        super(CountVisualizer, self).__init__()
+
 class CombineVisualizer(StageVisualizer):
     """Visualization for CONCUSS combination stage"""
+
+    def __init__(self):
+        super(CombineVisualizer, self).__init__()
 
