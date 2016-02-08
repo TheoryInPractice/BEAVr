bae72b62128fe9793e5736970bcbdb118ab08f24
Clayton G. Hobbs <cghobbs@ncsu.edu>
2016-02-08 11:09:26 -0500

    Added our wxPython application subclass

---
+++ drgraph/visapplication.py
@@ -0,0 +1,7 @@
+import wx
+
+class VisApplication(wx.App):
+    """The wxPython application class for the visualization tool"""
+
+    def OnInit(self):
+        """Set and show the application's main window"""
