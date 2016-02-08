2c236eb368a89c89ed4f4d19898f7f047f675aa3
Nishant Rodrigues <ngrodrig@ncsu.edu>
2016-02-08 10:41:50 -0500

    Added data loader files

---
+++ drgraph/concuss/concuss_data_loader.py
@@ -0,0 +1,10 @@
+from drgraph.data_loader import DataLoader
+
+class ConcussDataLoader(DataLoader):
+    """  Loads data provided by the CONCUSS pipeline """
+
+    def load(self):
+        """
+        Load the data
+        :return:
+        """
---
+++ drgraph/data_loader.py
@@ -0,0 +1,19 @@
+from abc import ABCMeta, abstractmethod
+
+class DataLoader(object):
+    """ Abstract Data Loader """
+    __metaclass__ = ABCMeta
+
+    def __init__(self, config_json):
+        """
+        Get the json configuration loaded by the DataLoaderFactory
+        :param config_json: The loaded json configuration
+        """
+        self.config_json = config_json
+
+    @abstractmethod
+    def load(self):
+        """
+        Load data
+        """
+        pass
