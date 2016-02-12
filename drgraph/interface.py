08aeff9104c841c92c1c1b8cbf45a43fafa58675
Clayton G. Hobbs <cghobbs@ncsu.edu>
2016-02-12 11:50:20 -0500

    Added a dummy StageInterface
    It shows a toolbar with an open button, and a blank white visualization
    panel.

--- drgraph/interface.py
+++ drgraph/interface.py
@@ -17,6 +17,11 @@ class MainInterface(wx.Frame):
         self._make_menubar()
         self.CreateStatusBar()
 
+        self.notebook = wx.Notebook(self, wx.NewId(), style=wx.BK_DEFAULT)
+
+        dummy = DummyStageInterface(self.notebook)
+        self.notebook.AddPage(dummy, "No Visualization")
+
         self.Center()
 
     def _make_menubar(self):
@@ -60,7 +65,43 @@ class StageInterface(wx.Panel):
     """
     __metaclass__ = ABCMeta
 
+    tb_size = (24, 24)
+
     @abstractmethod
     def __init__(self, parent):
         """Create a sizer with a toolbar and a space for the StageVisualizer"""
         super(StageInterface, self).__init__(parent)
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.tb = wx.ToolBar(self, style=wx.TB_VERTICAL)
+
+        self.vis = wx.Panel(self, -1)
+
+        sizer.Add(self.tb, 0, wx.EXPAND)
+        sizer.Add(self.vis, 1, wx.EXPAND)
+
+        self.SetSizer(sizer)
+
+    def set_visualization(self, vis):
+        """Set the panel to be used for visualization"""
+        self.vis = vis
+
+class DummyStageInterface(StageInterface):
+    """
+    A blank StageInterface.
+
+    The DummyStageInterface has an open button in its toolbar and a blank white
+    visualization.
+    """
+
+    def __init__(self, parent):
+        """Create a StageInterface with nothing special added"""
+        super(DummyStageInterface, self).__init__(parent)
+
+        open_bmp = wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_TOOLBAR,
+                self.tb_size)
+        self.tb.AddTool(wx.ID_OPEN, open_bmp)
+
+        self.tb.Realize()
+
+        self.vis.SetBackgroundColour(wx.WHITE)
