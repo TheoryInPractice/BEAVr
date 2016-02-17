21d091b6aa72a56b28c90480054dd1070727413d
Brandon Mork <bpmork@ncsu.edu>
2016-02-17 14:22:30 -0500

    Added Factory.create to concuss.data_loader
    Factory.create will be the generic code in all pipeline.data_loader
    script that data_loader.DataLoaderFactory will use to instantiate
    PipelineDataLoader objects.

--- drgraph/concuss/data_loader.py
+++ drgraph/concuss/data_loader.py
@@ -1,8 +1,17 @@
 from drgraph.data_loader import DataLoader
 
+class Factory:
+    """
+    Generically named wrapper for creating ConcussDataLoader via
+    data_loader.DataLoaderFactory
+    """
+    
+    def create():
+        return ConcussDataLoader()
+
 
 class ConcussDataLoader(DataLoader):
-    """  Loads data provided by the CONCUSS pipeline """
+    """ Loads data provided by the CONCUSS pipeline """
 
     def load(self):
         """
