b0cdce631c4cb1e039ef3ce7221343ac6c5aabb4
Brandon Mork <bpmork@ncsu.edu>
2016-02-17 11:31:02 -0500

    Implemented DataLoaderFactory.data_loader
    Mostly complete implementation. Need to ensure I am correctly importing
    pipeline.data_loader correctly from string value.

--- drgraph/data_loader.py
+++ drgraph/data_loader.py
@@ -1,4 +1,7 @@
 from abc import ABCMeta, abstractmethod
+from zipfile import ZipFile
+import ConfigParser
+import os.path
 
 class DataLoader(object):
     """ Abstract Data Loader """
@@ -24,3 +27,23 @@ class DataLoaderFactory(object):
 
     def data_loader(self, filename):
         """Create the appropriate DataLoader for the given filename"""
+
+        # Open zip archive as ZipFile object
+        archive = ZipFile( filename, 'r' )
+        # dir_name is filename without extension
+        dir_name = filename.split('.')[0]
+
+        # Create config parser.
+        parser = ConfigParser.ConfigParser()
+        # Parse visinfo.cfg for pipeline name
+        parser.readfp( myzip.open( dir_name + '/visinfo.cfg', 'r' ) )
+        pipeline_name = parser.get( 'pipeline','name' )
+
+        # Check if data_loader.py exists in directory for the given pipeline
+        if not os.path.exists( './' + pipeline_name + '/data_loader.py' ):
+            sys.exit( './' + pipeline_name + '/data_loader.py missing.\n' )
+
+        # Create and return data_loader object for pipeline
+        ## TODO
+        ## from importlib import import_module
+        ## pipe_data_loader = import_module( pipeline_name + '/data_loader.py' )
