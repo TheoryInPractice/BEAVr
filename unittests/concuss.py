82eb80d37330b32e5dae6399d1d0cff27c846a51
Clayton G. Hobbs <cghobbs@ncsu.edu>
2016-03-23 10:10:54 -0400

    Shorten a docstring to fit in the proper length

--- unittests/concuss.py
+++ unittests/concuss.py
@@ -4,10 +4,10 @@ from drgraph.concuss import visualizerbackend
 class TestDecompositionGenerator(unittest.TestCase):
 
     def setUp(self):
-        """ Sets up the necessary data structures and variables before tests are run """
+        """Sets up the necessary objects before running tests"""
 
     def tearDown(self):
-        """ Cleans up after tests are run """
+        """Cleans up after tests are run"""
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDecompositionGenerator)
 
