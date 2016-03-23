4ec38c2f35108156db956e63cc60d979f171e30a
Clayton G. Hobbs <cghobbs@ncsu.edu>
2016-03-23 11:00:06 -0400

    Added a unit test for four_color_sets
    I know we're planning to replace four_color_sets with something that
    lets users select what colors they want to see, but as long as we have
    it we should have a test case for it.

--- unittests/concuss.py
+++ unittests/concuss.py
@@ -1,16 +1,30 @@
 import unittest
+
+import networkx as nx
+
 from drgraph.concuss import visualizerbackend
 
 class TestDecompositionGenerator(unittest.TestCase):
 
     def setUp(self):
         """ Sets up the necessary objects to run"""
-        graph = nx.Graph()
-        graph.add_nodes_from(range(6))
-        graph.add_edges_from([(0,1), (1,2), (1,3), (2,3), (3,4), (4,5)])
-        coloring = range(6)
+        self.graph = nx.Graph()
+        self.graph.add_nodes_from(range(6))
+        self.graph.add_edges_from([(0,1), (1,2), (1,3), (2,3), (3,4), (4,5)])
+        self.coloring = range(6)
 
-        decomp_generator = DecompositionGenerator(graph, coloring)
+        self.decomp_generator = visualizerbackend.DecompositionGenerator(
+                self.graph, self.coloring)
+
+    def test_four_color_sets(self):
+        p = 3
+        # Make four color sets of size p
+        sets = self.decomp_generator.four_color_sets(set(self.coloring), p)
+        # Assert that we got four sets
+        self.assertEquals(len(sets), 4)
+        # Assert that each set has size p
+        for s in sets:
+            self.assertEquals(len(s), p)
 
     def tearDown(self):
         """Cleans up after tests are run"""
