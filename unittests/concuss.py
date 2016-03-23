c4e060567a036ac62ecdf25ea15c717cd8e2d788
yho <yho@ncsu.edu>
2016-03-23 10:38:55 -0400

    Added general test module.py

--- unittests/concuss.py
+++ unittests/concuss.py
@@ -5,6 +5,12 @@ class TestDecompositionGenerator(unittest.TestCase):
 
     def setUp(self):
         """ Sets up the necessary data structures and variables before tests are run """
+        graph = nx.Graph()
+        graph.add_nodes_from(range(6))
+        graph.add_edges_from([(0,1), (1,2), (1,3), (2,3), (3,4), (4,5)])
+        coloring = range(6)
+
+        decomp_generator(graph, coloring)
 
     def tearDown(self):
         """ Cleans up after tests are run """
---
+++ unittests/general.py
@@ -0,0 +1,25 @@
+import unittest
+from drgraph import util
+
+class TestUtil(unittest.TestCase):
+
+    def setUp(self):
+        """ Sets up the necessary data structures and variables before tests are run """
+        palette_name = ""
+
+    def tearDown(self):
+        """ Cleans up after tests are run """
+
+    def test_load_palette(self):
+        """ Tests load_palette """
+
+    def test_map_colorings(self):
+        """ Tests map_colorings"""
+
+    def test_map_coloring(self):
+        """ Tests map_coloring"""
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestUtil)
+
+if __name__ == '__main__':
+    unittest.main()
