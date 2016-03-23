62446eea863af8167226e4bc8aad05ae932d28f0
yho <yho@ncsu.edu>
2016-03-23 10:04:24 -0400

    Added a unit test directory, no test cases have been written

---
+++ unittests/concuss.py
@@ -0,0 +1,15 @@
+import unittest
+from drgraph.concuss import visualizerbackend
+
+class TestDecompositionGenerator(unittest.TestCase):
+
+    def setUp(self):
+        """ Sets up the necessary data structures and variables before tests are run """
+
+    def tearDown(self):
+        """ Cleans up after tests are run """
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestDecompositionGenerator)
+
+if __name__ == '__main__':
+    unittest.main()
