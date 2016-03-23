aec8143aeb39422ab25341db057bc34e56724494
Clayton G. Hobbs <cghobbs@ncsu.edu>
2016-03-23 11:19:41 -0400

    Added a test for get_connected_components

--- unittests/concuss.py
+++ unittests/concuss.py
@@ -21,10 +21,22 @@ class TestDecompositionGenerator(unittest.TestCase):
         # Make four color sets of size p
         sets = self.decomp_generator.four_color_sets(set(self.coloring), p)
         # Assert that we got four sets
-        self.assertEquals(len(sets), 4, msg='Wrong number of sets returned')
+        self.assertEqual(len(sets), 4, msg='Wrong number of sets returned')
         # Assert that each set has size p
         for s in sets:
-            self.assertEquals(len(s), p, msg='Wrong number of colors in a set')
+            self.assertEqual(len(s), p, msg='Wrong number of colors in a set')
+
+    def test_get_connected_components(self):
+        # Get the components on set {0, 1, 2}
+        cs = {0, 1, 2}
+        comps = self.decomp_generator.get_connected_components(cs)
+        # Assert that we have one component on this color set
+        self.assertEquals(len(comps), 1, msg='Wrong number of components')
+        # Assert that it has vertices [0, 1, 2]
+        self.assertEquals(comps[0].nodes(), [0, 1, 2], msg='Wrong vertex set')
+        # Assert that it has edges [(0, 1), (1, 2)]
+        self.assertEquals(comps[0].edges(), [(0, 1), (1, 2)],
+                msg='Wrong edge set')
 
     def tearDown(self):
         """Cleans up after tests are run"""
